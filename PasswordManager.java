6076bdcf65ff5ebfbe54abaafdf4c92565a643de
Grant <grant.cieslack@evsck12.com>
2021-11-18 12:00:08 -0600

    todo

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,5 +1,22 @@
 public class PasswordManager {
     public static void main(String[] args){
-        
+        /* TODO:
+            Account
+                whereIsUsed & username & password
+            PasswordHandler
+                passwordChecker
+                passwordGenerator
+            logIn/acctCreation
+                check for pre-existing profile
+                if none exists, create new
+                    first & last name, username, password, make a hint for your login
+                    login
+                if exists, login
+                    username & password
+                    3 tries
+                    hint happens
+                    2 tries
+                    close app
+        */
     }
 }
